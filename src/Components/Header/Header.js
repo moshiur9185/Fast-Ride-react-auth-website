a8903b1f998136353a742b74cfdf0fe0adf14f41
moshiur9185 <moshiur9185@gmail.com>
2022-02-23 14:06:19 +0600

    work modify

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -23,12 +23,12 @@ const Header = () => {
     return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-                <a className="navbar-brand" href="/home">Fast Ride</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <a className="navbar-brand fw-bold" href="/home">Fast Ride</a>
+            <button className="navbar-toggler btn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mx-auto">
+                <ul className="navbar-nav fw-bold ms-auto">
                     <Link style={{ textDecoration: 'none' }} to="/home"><li className="nav-item active mr-3 p-1">
                         <p className="nav-link text-dark">Home </p>
                     </li></Link>
@@ -42,8 +42,8 @@ const Header = () => {
                         <a className="nav-link text-dark" href="/home">Contects</a>
                     </li>
                     {
-                        loggedInUser.email || isLogged ? <button style={{ backgroundColor: "#111430", textDecoration: "none", color: "white" }} className="nav-item btn px-4 h-75  bg-secondary" onClick={signOut}> {loggedInUser.name || loggedUser.name ? loggedInUser.name || loggedUser.name : loggedInUser.displayName || loggedUser.displayName}</button> :
-                        <Link to="/login"><button style={{ backgroundColor: "#111430", textDecoration: "none", color: "white" }} className="nav-item btn px-4 h-75 bg-primary btn-success">Sign In</button></Link>
+                        loggedInUser.email || isLogged ? <button style={{ backgroundColor: "#111430", textDecoration: "none", color: "white" }} className="nav-item btn bg-secondary mb-2" onClick={signOut}> {loggedInUser.name || loggedUser.name ? loggedInUser.name || loggedUser.name : loggedInUser.displayName || loggedUser.displayName}</button> :
+                        <Link to="/login"><button style={{ backgroundColor: "#111430", textDecoration: "none", color: "white" }} className="nav-item btn bg-secondary mt-2">Sign In</button></Link>
                     }       
                 </ul>
             </div>
